fcc3244797f6437a42d25736283fd7276b51528b
xavier.huang <xavier.huang@chel-c.com>
2018-12-25 14:18:26 +0800

    基础功能完成

--- app/src/main/java/com/allsunny/zhihudailykotlin/bean/NewsBean.kt
+++ app/src/main/java/com/allsunny/zhihudailykotlin/bean/NewsBean.kt
@@ -6,25 +6,26 @@ package com.allsunny.zhihudailykotlin.bean
  * Email: 623368886@qq.com
  * Desc:
  */
- 
+
 data class NewsBean(
-    val date: String,
-    val stories: List<Story>,
-    val top_stories: List<TopStory>
+        val date: String,
+        val stories: List<Story>,
+        val top_stories: List<TopStory>
 )
 
 data class Story(
-    val ga_prefix: String,
-    val id: Int,
-    val images: List<String>,
-    val title: String,
-    val type: Int
+        val ga_prefix: String,
+        val id: Int,
+        val images: List<String>,
+        val title: String,
+        val type: Int,
+        val multipic: Boolean
 )
 
 data class TopStory(
-    val ga_prefix: String,
-    val id: Int,
-    val image: String,
-    val title: String,
-    val type: Int
+        val ga_prefix: String,
+        val id: Int,
+        val image: String,
+        val title: String,
+        val type: Int
 )
--- app/src/main/java/com/allsunny/zhihudailykotlin/ui/MainActivity.kt
+++ app/src/main/java/com/allsunny/zhihudailykotlin/ui/MainActivity.kt
@@ -5,6 +5,8 @@ import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.RecyclerView.*
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
@@ -23,12 +25,14 @@ import com.orhanobut.logger.Logger
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import android.widget.Toast
+import com.allsunny.zhihudailykotlin.bean.NewsBean
 
 
 class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     private var newsListAdapter: BaseQuickAdapter<Story, BaseViewHolder>? = null
     private var newsListData: List<Story>? = null
+    private lateinit var currentDate: String
 
     private val linearLayoutManager by lazy {
         LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
@@ -42,9 +46,9 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
         setSupportActionBar(toolbar)
 
         val toggle = ActionBarDrawerToggle(
-            this, drawer_layout, toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
+                this, drawer_layout, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
         )
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
@@ -71,20 +75,25 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
                 helper!!.setText(R.id.tv_title, item!!.title)
 
                 Glide.with(mContext)
-                    .load(item.images[0])
-                    .error(R.mipmap.ic_launcher)           //设置错误图片
-                    .placeholder(R.mipmap.ic_launcher)     //设置占位图片
+                        .load(item.images[0])
+                        .error(R.mipmap.ic_launcher)           //设置错误图片
+                        .placeholder(R.mipmap.ic_launcher)     //设置占位图片
 //                    .dontAnimate()                           //解决Glide加载图片的变形问题
-                    .diskCacheStrategy(DiskCacheStrategy.RESULT) //缓存
-                    .into(helper.getView(R.id.iv_story))
+                        .diskCacheStrategy(DiskCacheStrategy.RESULT) //缓存
+                        .into(helper.getView(R.id.iv_story))
 
                 helper.addOnClickListener(R.id.cv_item)
 
+                if (item.multipic) {
+                    helper.setVisible(R.id.iv_multi_pic, true)
+                } else {
+                    helper.setVisible(R.id.iv_multi_pic, false)
+                }
+
                 helper.setOnClickListener(R.id.cv_item, View.OnClickListener {
                     val intent = Intent(mContext, NewsDetailActivity::class.java)
-                    intent.putExtra("news_id",item.id)
+                    intent.putExtra("news_id", item.id)
                     startActivity(intent)
-//                    ToastUtil.showToast(item!!.title)
                 })
 
             }
@@ -92,23 +101,52 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
 
         rv_data.layoutManager = linearLayoutManager
         rv_data.adapter = newsListAdapter
+        /**
+         * object:实现匿名内部类
+         */
+        rv_data.addOnScrollListener(object : OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                val totalItemCount = linearLayoutManager.getItemCount()
+                val lastVisibleItem = linearLayoutManager.findLastVisibleItemPosition()
+                if (lastVisibleItem > totalItemCount - 3 && dy > 0) {
+                    getBeforeNews(currentDate)
+                }
+
+            }
+        })
 
         getLastNews()
     }
 
     private fun getLastNews() {
         RetrofitManager.service.getNewsData()
-            .subscribeOn(Schedulers.io())
-            .observeOn(AndroidSchedulers.mainThread())
-            .subscribe({ newsBean ->
-                newsListData = newsBean.stories
-                newsListAdapter!!.setNewData(newsListData)
-                if (swr_refresh.isRefreshing) {
-                    swr_refresh.setRefreshing(false)
-                }
-            }, { throwable ->
-                Logger.e(throwable.toString())
-            })
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ newsBean: NewsBean? ->
+                    newsListData = newsBean?.stories
+                    newsListAdapter?.setNewData(newsListData)
+                    currentDate = newsBean?.date.toString()
+                    if (newsListData!!.size < 8) {
+                        getBeforeNews(currentDate)
+                    }
+                    if (swr_refresh.isRefreshing) {
+                        swr_refresh.setRefreshing(false)
+                    }
+                }, { throwable: Throwable? ->
+                    Logger.e(throwable.toString())
+                })
+    }
+
+    private fun getBeforeNews(date: String) {
+        RetrofitManager.service.getBeforeNews(date)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ newsBean: NewsBean? ->
+                    newsListAdapter?.addData(newsBean!!.stories)
+                    currentDate = newsBean?.date.toString()
+                }, { throwable: Throwable? ->
+                    Logger.e(throwable.toString())
+                })
     }
 
     override fun onBackPressed() {
@@ -135,8 +173,8 @@ class MainActivity : BaseActivity(), NavigationView.OnNavigationItemSelectedList
                 return true
             }
             R.id.action_settings -> return true
-            R.id.action_about->{
-                startActivity(Intent(this,AboutActivity::class.java))
+            R.id.action_about -> {
+                startActivity(Intent(this, AboutActivity::class.java))
                 return true
             }
             else -> return super.onOptionsItemSelected(item)
